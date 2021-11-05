e4d227cedb5f7abb8eeeae48263ec51047b63e7c
dhaniemubarak <dhaniemubarak@gmail.com>
2021-11-05 11:48:00 +0700

    HOL-61 Init project

---
+++ src/main/java/id/holigo/services/holigooauthservice/HoligoOauthServiceApplication.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigooauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoligoOauthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoligoOauthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/holigo/services/holigooauthservice/HoligoOauthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigooauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoligoOauthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
